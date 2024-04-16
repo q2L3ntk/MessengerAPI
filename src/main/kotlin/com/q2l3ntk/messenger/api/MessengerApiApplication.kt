9f3a5b542c6c8babca7fc6385cf3ea88529198e3
MatreXiXesS <zurskij@gmail.com>
2024-04-16 23:50:46 +0300

    Created API for Messenger application

---
+++ src/main/kotlin/com/q2l3ntk/messenger/api/MessengerApiApplication.kt
@@ -0,0 +1,11 @@
+package com.q2l3ntk.messenger.api
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class MessengerApiApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(MessengerApiApplication::class.java, *args)
+}
---
+++ src/test/kotlin/com/q2l3ntk/messenger/api/MessengerApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.q2l3ntk.messenger.api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MessengerApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
