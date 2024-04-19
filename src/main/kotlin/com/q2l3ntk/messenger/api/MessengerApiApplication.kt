c709d9522f51bdb7c1d010868f00c39b92598a67
MatreXiXesS <zurskij@gmail.com>
2024-04-19 22:14:59 +0300

    Change in main function application

--- src/main/kotlin/com/q2l3ntk/messenger/api/MessengerApiApplication.kt
+++ src/main/kotlin/com/q2l3ntk/messenger/api/MessengerApiApplication.kt
@@ -1,11 +1,11 @@
 package com.q2l3ntk.messenger.api
 
-import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
 
 @SpringBootApplication
 class MessengerApiApplication
 
 fun main(args: Array<String>) {
-    SpringApplication.run(MessengerApiApplication::class.java, *args)
+    runApplication<MessengerApiApplication>(*args)
 }
